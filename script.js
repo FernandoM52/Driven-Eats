33850b5e305ce94384baa25a6d6480954b355d96
FernandoM52 <fernandomartins.contato@outlook.com>
2023-01-09 06:16:35 -0300

    botao pedido habilitado

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-function selecionarPrato(pratoSelecionado, tipo) {
+function selecionarPrato(itemSelecionado, tipo) {
     const validarPratoSelecionado = document.querySelector(`.${tipo} .ativo`);
 
-    //verificar se ja tem algum prato selecionado
+    //verificar se ja tem algum item selecionado
     if (validarPratoSelecionado !== null) {
         validarPratoSelecionado.classList.remove("ativo")
         validarPratoSelecionado.classList.remove("bordaSelecionado");
     }
 
-    //inserir borda e check ao clicar no prato
-    pratoSelecionado.classList.add("ativo");
-    pratoSelecionado.classList.add("bordaSelecionado");
+    //inserir borda e check ao clicar no item
+    itemSelecionado.classList.add("ativo");
+    itemSelecionado.classList.add("bordaSelecionado");
+
+    //função valida botao de fechar pedido
+    habilitaBotaoFecharPedido();
 }
+
+function habilitaBotaoFecharPedido() {
+    const botao = document.querySelector('footer .botao');
+    if (document.querySelectorAll('.ativo').length === 3) {
+        botao.disabled = false;
+        botao.classList.add('botaoHabilitado');
+        botao.innerHTML = "Fechar pedido";
+    }
+}
+
